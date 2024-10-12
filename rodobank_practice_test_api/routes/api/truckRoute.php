041f172dd3c5e3e794c7278bb5603a3474268cb1
Vitor Gabriel <dev.vitor.gabriel@gmail.com>
2024-10-12 03:27:54 -0300

    feat: criando rotas da api;

---
+++ rodobank_practice_test_api/routes/api/truckRoute.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\API\TruckController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::controller(TruckController::class)->group(function () {
+    Route::get('/', 'index');
+    Route::get('/{id}', 'show');
+    Route::post('/', 'store');
+    Route::put('/{id}', 'update');
+    Route::delete('/{id}', 'destroy');
+});
