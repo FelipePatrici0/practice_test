21bc62e0a60cca233fbc2e637bc99a733b120305
Vitor Gabriel <dev.vitor.gabriel@gmail.com>
2024-10-11 23:06:32 -0300

    feat: criando migration da transportadora

---
+++ rodobank_practice_test_api/database/migrations/2024_10_12_020206_create_tb_carrier.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTbCarrierTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('tb_carrier', function (Blueprint $table) {
+            $table->id('id_carrier_tbc');
+            $table->string('name_carrier_tbc', 100);
+            $table->char('cnpj_carrier_tbc', 14);
+            $table->boolean('is_active_tbc')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('tb_carrier');
+    }
+}
+
