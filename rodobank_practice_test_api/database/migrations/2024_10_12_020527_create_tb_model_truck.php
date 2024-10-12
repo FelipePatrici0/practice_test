2709b737d23ce00c79a5ea3b87ff6e3d967ebe81
Vitor Gabriel <dev.vitor.gabriel@gmail.com>
2024-10-11 23:07:31 -0300

    feat: criando migration do modelo do caminhao;

---
+++ rodobank_practice_test_api/database/migrations/2024_10_12_020527_create_tb_model_truck.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTbModelTruckTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('tb_model_truck', function (Blueprint $table) {
+            $table->id('id_model_truck_tmt');
+            $table->string('model_truck_tmt', 50);
+            $table->string('color_truck_tmt', 50);
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('tb_model_truck');
+    }
+}
+
