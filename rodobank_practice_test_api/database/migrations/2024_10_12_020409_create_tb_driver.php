9b1fb117427c6c968bed01272e5251c97c4985aa
Vitor Gabriel <dev.vitor.gabriel@gmail.com>
2024-10-11 23:06:46 -0300

    feat: criando a migration do motorista

---
+++ rodobank_practice_test_api/database/migrations/2024_10_12_020409_create_tb_driver.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTbDriverTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('tb_driver', function (Blueprint $table) {
+            $table->id('id_driver_tbd');
+            $table->string('name_driver_tbd', 100);
+            $table->char('cpf_driver_tbd', 11);
+            $table->date('birthdate_driver_tbd');
+            $table->string('email_driver_tbd', 100)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('tb_driver');
+    }
+}
+
